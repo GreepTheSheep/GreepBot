6740efe4019795ca4bf868b8bdc41fbff53bb77d
Greep <42576124+GreepTheSheep@users.noreply.github.com>
2019-02-08 21:40:01 +0100

    Ajout des commandes /google et /youtube. Crédit : Tryliom

--- bot.js
+++ bot.js
@@ -13,6 +13,11 @@ client.on('message', message => {
     const ping = require('./cmds/ping.js');
     ping(message, client, prefix);
 
+    const google = require('./cmds/google.js');
+    google(message, client, prefix);
+
+    const youtube = require('./cmds/youtube.js');
+    youtube(message, client, prefix);
 });
 
 client.login(config.token);
