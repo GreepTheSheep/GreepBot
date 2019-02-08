4a4da05a371bee43e4a2cb1ccc0bdcea34502f1a
Greep <42576124+GreepTheSheep@users.noreply.github.com>
2019-02-08 21:40:29 +0100

    Create youtube.js
    Crédit : Tryliom

---
+++ cmds/youtube.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+function youtube(message, client, prefix) {
+
+    if (message.content.startsWith(prefix + 'yt')) {
+        let args = message.content.split(" ")
+        args.shift()
+        message.delete()
+        let embed = new Discord.RichEmbed()
+        embed.setColor("#00ff00")
+            .setTimestamp()
+            .addField("Voila votre recherche", "Recheche [Youtube]: \n https://www.youtube.com/results?search_query=" + args.join("%20"))
+            .setThumbnail("https://media.giphy.com/media/2LA53y7maD8is/giphy.gif")
+            .setFooter("Recherche YT ", "https://media.giphy.com/media/2LA53y7maD8is/giphy.gif")
+            .setFooter("GreepBot : Restez toujours un 🐑! | ", `${client.user.avatarURL}`)
+        message.channel.sendMessage({ embed: embed });
+    }
+}
+
+module.exports = youtube;
