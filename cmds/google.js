ece979dde0ae6f3abb7e8d6c9b6b91189c53fe4c
Greep <42576124+GreepTheSheep@users.noreply.github.com>
2019-02-09 09:53:41 +0100

    Changement de la recherche : "J'ai de la chance !"

--- cmds/google.js
+++ cmds/google.js
@@ -9,7 +9,7 @@ function google(message, client, prefix) {
         let embed = new Discord.RichEmbed()
         embed.setColor("#00ff00")
             .setTimestamp()
-            .addField("Voila votre recherche", "Recheche [Google]: \n https://www.google.fr/#q=" + args.join("%20"))
+            .addField("Voila votre recherche", "Recheche [Google]: \n https://www.google.fr/#q=" + args.join("%20") + "&btnI=I%27m+Feeling+Lucky")
             .setThumbnail("http://www.slate.com/content/dam/slate/blogs/future_tense/2015/09/01/google_s_new_animated_logo_is_the_future_of_mobile_branding/google_logo.gif.CROP.original-original.gif")
             .setFooter("Recherche Google ", "http://www.slate.com/content/dam/slate/blogs/future_tense/2015/09/01/google_s_new_animated_logo_is_the_future_of_mobile_branding/google_logo.gif.CROP.original-original.gif")
             .setFooter("GreepBot : Restez toujours un 🐑! | ", `${client.user.avatarURL}`)
